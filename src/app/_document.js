a9ac4c647212b8cd2ce6a992a6a2dc4e8b208df5
KUJ <canpc1997@gmail.com>
2025-01-23 03:06:35 +0900

    20250123-2

--- src/app/_document.js
+++ src/app/_document.js
@@ -1,11 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        {/* 카카오 JavaScript SDK */}
-        <script src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
+        {/* 카카오 JavaScript SDK 비동기 로드 */}
+        <Script
+          src="https://developers.kakao.com/sdk/js/kakao.min.js"
+          strategy="lazyOnload"
+        />
       </Head>
       <body>
         <Main />
